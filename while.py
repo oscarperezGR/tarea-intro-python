3a297b23bdbd248021f89d254c18804e7ca58deb
oscarperezGR <oscarperezlds5@gmail.com>
2026-09-23 00:05:35 +0200

    feat: add select status piece

--- while.py
+++ while.py
@@ -85,28 +85,32 @@ catalog = {
 
 
 }
+print()
+print("*"* 40)
+print("Bienvenido o Bienvenida para al sistema de registro de pieza")
+print("Usted esta ingresando al catálogo de piezas coleccionables")
+print("*"* 40)
+print("***Catalogo Actual***",catalog)
 
-print("***Viejo catalogo***",catalog)
-"""
-cont = 1
-while cont <= recorrido:
-    for x, obj in catalog.items():
-        print(f"{x} : {catalog[x]}"  )
-    cont+=1
-"""
 recorrido=0
 recorrido = int
 cont1=1
-
-while cont1 < 3:
-                
-                recorrido= input("cuantas piezas quieres añadir? Maximo de 10 piezas: ")
+while True:
+        if cont1 == 4:
+                print("Cerrando programa")
+                exit()
+        else:
+                recorrido= input("¿Cuantas piezas quieres añadir? *Maximo de 10 piezas*: ")
                 recorrido=int(recorrido)
-                if recorrido < 10:
-                        cont1= 3
-                else:
-                        print("Tiene que ser menor de 10 piezas a añadir")
+                if      recorrido <= 10:
+                        break
+                elif recorrido > 10 or recorrido < 0:
+                        (print("Tiene que ser menor de 10 piezas a añadir"))
                         cont1+=1
+
+
+
+
 
 cont2 = 1
 while cont2 <= recorrido:
@@ -120,9 +124,20 @@ while cont2 <= recorrido:
         categot = input("ingreso la categoria " + str(cont2) + " : ")
         catalog[f"piezas{cont2}"]["category"]=categot
         print("")
-        price = input("ingreso el precio de la pieza " + str(cont2) + " : ")
-        catalog[f"piezas{cont2}"]["price"] = price
-        print("")
+
+        while True:
+                price = input("Ingrese el precio de la pieza " + str(cont2) + " : ")
+
+                try:
+                        price = int(price)
+                        if price > 0:
+                                catalog[f"piezas{cont2}"]["price"] = price
+                                print()
+                                break
+                        else:
+                                print("***El precio debe ser mayor a cero***")
+                except:
+                        print("***Ingrese un precio válido***")
         while True:
                 print("Opciones de las piezas")
                 print("1. disponible")
@@ -131,25 +146,40 @@ while cont2 <= recorrido:
                 status_piece = input("Solo ingrese una opcion del 1 al 3 de la pieza " + str(cont2) + " : ")
                 if status_piece == "1":
                         status_piece = str(status_piece)
-                        status_available = status_piece
+                        status_available = "disponible"
                         catalog[f"piezas{cont2}"]["status"] = status_available
                         break
                 elif status_piece == "2":
                         status_piece = str(status_piece)
-                        status_available = status_piece
+                        status_available = "reservada"
                         catalog[f"piezas{cont2}"]["status"] = status_available
                         break
                 elif status_piece == "3":
                         status_piece = str(status_piece)
-                        status_available = status_piece
+                        status_available = "vendida"
                         catalog[f"piezas{cont2}"]["status"] = status_available
                         break
                 else:
                         print("Opcion invalida, ingrese opcion del 1 al 3")
 
         print("")
-        description_piece = input("ingreso la categoria " + str(cont2) + " : ")
-        catalog[f"piezas{cont2}"]["description"] = description_piece
+        while True:
+                print("*¿Cual de los dos estados siguientes describe mejor su pieza?*")
+                print("1. usada")
+                print("2. certificada")
+                description_piece = input("Seleccione un estado (1 - 2) y agregue algo mas de la pieza" + str(cont2) + " : ")
+
+                if description_piece.startswith("1"):
+                        description_piece = str(description_piece)
+                        catalog[f"piezas{cont2}"]["description"] = "pieza usada " + description_piece
+                        break
+
+                if description_piece.startswith("2"):
+                        description_piece = str(description_piece)
+                        catalog[f"piezas{cont2}"]["description"] = "pieza certificada " + description_piece
+                        break
+                else:
+                        print("Opcion invalida, ingrese opcion del 1 al 2")
         cont2+=1
 print()
 print("Catalogo actualizado: ", catalog)
